98469916c0ad74f0e5ff2a8c8335beb773fca8c5
Erbol <erboshaazhybekov@gmail.com>
2022-06-14 13:04:27 +0600

    accountmodel

---
+++ apps/account/serializers.py
@@ -0,0 +1,53 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+
+User = get_user_model()  # внутри лежит AUTH_USER_MODEL
+
+
+class RegistrationSerializer(serializers.Serializer):
+    username = serializers.CharField(max_length=100)
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(min_length=6, required=True)
+    password_confirm = serializers.CharField(min_length=6, required=True)
+
+    def validate_email(self, email):
+        if User.objects.filter(email=email).exists():  # проверили имейл на соответствие базе данных перед сохранением
+            raise serializers.ValidationError('email already exists')
+        return email
+
+    def validate(self, attrs: dict):
+        pass1 = attrs.get('password')
+        pass2 = attrs.pop('password_confirm')  # удаляем поле чтобы оно не пошло в базу данных т.к. оно нам там не нужно
+        if pass1 != pass2:
+            raise serializers.ValidationError("passwords don't match")
+        return attrs
+
+    def save(self):
+        data = self.validated_data  # сюда падает validate_email и validate
+        user = User.objects.create_user(**data)
+        user.set_activation_code()
+        user.send_activation_email()
+
+
+class LoginSerializer(TokenObtainPairSerializer):
+    email = serializers.EmailField(required=True)
+    password = serializers.CharField(min_length=6)
+
+    def validate_email(self, email):
+        if not User.objects.filter(
+                email=email).exists():  # проверили имейл на соответствие базе данных перед сохранением
+            raise serializers.ValidationError("email doesn't exists")
+        return email
+
+    def validate(self, attrs):
+        email = attrs.get('email')
+        password = attrs.pop('password')
+        user = User.objects.get(email=email)
+        if not user.check_password(password):
+            raise serializers.ValidationError('Invalid password')
+        if user and user.is_active:
+            refresh = self.get_token(user)
+            attrs['refresh'] = str(refresh)
+            attrs['access'] = str(refresh.access_token)
+        return attrs
--- apps/account/views.py
+++ apps/account/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.generics import get_object_or_404
+from .serializers import RegistrationSerializer, LoginSerializer
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+User = get_user_model()
+
+
+class RegistrationView(APIView):
+    def post(self, request):
+        serializer = RegistrationSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            message = "User registered!"
+            return Response(message)
+
+
+class ActivationView(APIView):
+    def get(self, request, activation_code):
+        user = get_object_or_404(User, activation_code=activation_code)
+        user.is_active = True
+        user.activation_code = ''
+        user.save()
+        return Response('your account is sucessfully activated!', status=status.HTTP_200_OK)
+
+
+class LoginView(TokenObtainPairView):
+    serializer_class = LoginSerializer
