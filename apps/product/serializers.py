422843c801860d943eb0fe76147a08bca33b0d71
Kalyia <bekieva934@gmail.com>
2022-06-16 11:14:31 +0600

    test

---
+++ apps/product/serializers.py
@@ -0,0 +1,83 @@
+from rest_framework import serializers
+from .models import Product, Review, LikeProduct, SimilarProduct
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    author = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    created_at = serializers.DateTimeField(format='%d/%m/%Y %H:%M:%S', read_only=True)
+
+    class Meta:
+        model = Product
+        exclude = ('create_date', 'update_date')
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['category'] = instance.category.name
+        representation['author'] = instance.author.email
+        representation['images'] = ProductImageSerializer(instance.images.all(),
+                                                  many=True, context=self.context).data
+        representation['reviews'] = ReviewProductSerializer(instance.reviews.all(),
+                                                  many=True, context=self.context).data
+
+        representation['likes'] = instance.likes.filter(is_like=True).count()
+        return representation
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['image'] = self._get_image_url(instance)
+        representation['product'] = instance.product.title
+        return representation
+
+
+class ReviewProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        exclude = ('author', )
+
+    def validate(self, attrs):
+        request = self.context.get('request')
+        attrs['author'] = request.user
+        return attrs
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['author'] = instance.author.email
+        representation['product'] = instance.product.title
+        return representation
+
+
+class LikeProductSerializer(serializers.ModelSerializer):
+    user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+
+    class Meta:
+        model = LikeProduct
+        fields = "__all__"
+
+
+class SimilarProductSerializer(serializers.ModelSerializer):
+    category = serializers.SerializerMethodField()
+    price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = SimilarProduct
+        fields = ['category', 'price']
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation['product'] = instance.product.title
+        return representation
+
+
+class ProductDetailSerializer(serializers.ModelSerializer):
+    category = serializers.SerializerMethodField()
+    products = SimilarProductSerializer(many=True)
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class FavoriteListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = '__all__' 
--- apps/product/views.py
+++ apps/product/views.py
@@ -1,3 +1,95 @@
+<<<<<<< Updated upstream
 from django.shortcuts import render
+=======
+from rest_framework import generics
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.filters import OrderingFilter, SearchFilter
+from rest_framework.decorators import action
+import django_filters.rest_framework as filters
+
+
+from .paginations import ProductPagination
+from .models import Product, Review, LikeProduct, Favorite
+from .serializers import *
+
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    filter_backends = (filters.DjangoFilterBackend, OrderingFilter, SearchFilter)
+    ordering_fields = ['create_date', 'title', 'price']
+    permission_classes = [IsAuthenticated, ]
+    pagination_class = ProductPagination
+    search_fields = ['title', 'description']
+
+    def get_serializer_class(self):
+        if self.action == 'list':
+            return ProductSerializer
+        return super().get_serializer_class()
+
+    def get_permissions(self):
+        if self.action in ['create', 'update', 'partial_update', 'destroy']:
+            return [IsAdminUser()]
+        elif self.action in ['toggle_like', ]:
+            return [IsAuthenticated()]
+        return []
+
+    # products/id/like/
+    @action(detail=True, methods=['GET'])
+    def like(self, request, pk):
+        product = self.get_object()
+        user = request.user
+        fav, created = LikeProduct.objects.get_or_create(product=product, user=user)
+        if fav.is_like == False:
+            fav.is_like = not fav.is_like
+            fav.save()
+            return Response('You liked this product')
+        else:
+            fav.is_like = not fav.is_like
+            fav.save()
+            return Response('You disliked this product')
+
+    # product/id/favorite/
+    @action(detail=True, methods=['GET'])
+    def favorite(self, request, pk):
+        product = self.get_object()
+        user = request.user
+        fav, created = Favorite.objects.get_or_create(product=product, user=user)
+        if fav.favorite == False:
+            fav.favorite = not fav.favorite
+            fav.save()
+            return Response('Added to Favs')
+        else:
+            fav.favorite = not fav.favorite
+            fav.save()
+            return Response('Not in Favs')
+
+
+class ReviewProductView(ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
+
+
+class ProductDetailView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductDetailSerializer
+
+
+class FavoriteView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = FavoriteListSerializer
+    permission_classes = [IsAuthenticated, ]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(favorites__user=self.request.user, favorites__favorite=True)
+        return queryset
+
+
+>>>>>>> Stashed changes
 
 # Create your views here.
