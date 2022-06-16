577e8fe698c429746cfa7b03ea45d06fc82cf967
Kalyia <bekieva934@gmail.com>
2022-06-16 17:04:33 +0600

    account/urls/update2

--- apps/account/urls.py
+++ apps/account/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import RegistrationView, ActivationView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
@@ -7,8 +7,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 urlpatterns = [
     
     path('register/', RegistrationView.as_view()),
-    path('api/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('activate/<str:activation_code>', ActivationView.as_view()),
 
 ]
