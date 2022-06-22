e5d52aa1168ea0d4d0647dd49459ad6765c2cb30
Kalyia <bekieva934@gmail.com>
2022-06-22 19:55:05 +0600

    .

--- apps/product/models.py
+++ apps/product/models.py
@@ -17,7 +17,7 @@ class Product(models.Model):
     category= models.CharField(choices=TYPES, max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    image = models.ImageField(upload_to='products', blank=True, null=True)
 
     def __str__(self):
         return self.title
